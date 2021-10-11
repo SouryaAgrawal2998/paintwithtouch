a18d610bfc6529fa9998772000ac9546d134a2a4
SouryaAgrawal2998 <81215092+SouryaAgrawal2998@users.noreply.github.com>
2021-10-12 00:12:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,43 @@
-var mouseevent = "";
-canvas = document.getElementById('myCanvas');
+var mouseevent = "empty";
+var last_position_of_touch_x, last_position_of_touch_y;
+var last_position_of_x, last_position_of_y;
+canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
-var color = "";
-var width_of_line = "";
-addEventListener("mousedown", my_mousedown);
+var color = "black";
+var width_of_line = 2;
+var width = screen.width;
+newWidth = screen.width - 70;
+newHeight = screen.height - 300;
+if (width < 992) {
+    document.getElementById("myCanvas").width = newWidth;
+    document.getElementById("myCanvas").height = newHeight;
+    document.body.style.overflow = "hidden";
+}
+else{
+    document.getElementById("myCanvas").width= "800";
+    document.getElementById("myCanvas").height= "600";
+}
+canvas.addEventListener("touchstart", my_touchstart);
+canvas.addEventListener("mousedown", my_mousedown);
 
 function my_mousedown() {
     color = document.getElementById("color").value;
     width_of_line = document.getElementById("widthOfLine").value;
-    mouseevent = "mouseup";
+    mouseevent = "mousedown";
 }
-//addEventListener("mouseup", my_mouseup);
+canvas.addEventListener("mouseup", my_mouseup);
 
 function my_mouseup() {
-    color = document.getElementById("color").value;
-   width_of_line = document.getElementById("widthOfLine").value;
-   mouseevent = "mouseup";
+    mouseevent = "mouseup";
 }
-//addEventListener("mouseleave", my_mouseleave);
+canvas.addEventListener("mouseleave", my_mouseleave);
 
 function my_mouseleave() {
-    color = document.getElementById("color").value;
-    width_of_line = document.getElementById("widthOfLine").value;
     mouseevent = "mouseleave";
 }
-addEventListener("mousemove", my_mousemove);
+canvas.addEventListener("mousemove", my_mousemove);
 
 function my_mousemove(e) {
-    color = document.getElementById("color").value;
-    width_of_line = document.getElementById("widthOfLine").value;
     mouseevent = "mousemove";
     current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
     current_position_of_mouse_y = e.clientY - canvas.offsetTop;
@@ -44,15 +53,24 @@ function my_mousemove(e) {
     last_position_of_y = current_position_of_mouse_y;
 }
 
+canvas.addEventListener("touchmove", my_touchmove);
+
 function my_touchmove(e) {
     current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
-    current_position_of_touch_y = e.touches[0].clienty - canvas.offsetTop;
+    current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
     ctx.beginPath();
-    ctx.strokeStyle =color;
+    ctx.strokeStyle = color;
     ctx.lineWidth = width_of_line;
     ctx.moveTo(last_position_of_touch_x, last_position_of_touch_y);
     ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
     ctx.stroke();
     last_position_of_touch_x = current_position_of_touch_x;
     last_position_of_touch_y = current_position_of_touch_y;
+}
+
+function my_touchstart(e) {
+    color = document.getElementById("color").value;
+    width_of_line = document.getElementById("widthOfLine").value;
+    last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
+    last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
 }
